4d58e126b9b5255e179bcef4776eaf999325f069
David Bosschaert <bosschae@adobe.com>
2022-12-09 11:04:01 +0000

    Make background color configurable

--- bundle/src/main/java/com/adobe/granite/demo/MyServlet.java
+++ bundle/src/main/java/com/adobe/granite/demo/MyServlet.java
@@ -9,23 +9,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardContextSelect;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 @HttpWhiteboardServletPattern("/myservlet") // overridden via configuration
 @HttpWhiteboardContextSelect("(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME +"=my-context)")
 @Component(service = Servlet.class, scope = ServiceScope.PROTOTYPE)
+@Designate(ocd=MyServlet.Config.class)
 public class MyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private String bgColor;
+
+    @Activate
+    protected void activate(Config config) {
+        bgColor = config.background();
+
+        System.out.println("*** Activated: " + config.background());
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
 
         resp.setContentType("text/html");
-        writer.write("<img src=\"www/site.jpg\" style=\"border: none; width: 100%;\"></img>");
+        writer.write("<body style=\"background-color:" + bgColor +
+            ";\"><img src=\"www/trees.png\" style=\"border: none; width: 100%;\"></img></body>");
+    }
+
+    @ObjectClassDefinition(name="Site settings")
+    public static @interface Config {
+        @AttributeDefinition(name="Background colour")
+        String background();
     }
 }
